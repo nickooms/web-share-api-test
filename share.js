06d989eec521a81347a3da032103992270610627
Nick Ooms <oomsni@gmail.com>
2020-10-03 05:14:41 +0200

    added target.html & .prettierrc

--- share.js
+++ share.js
@@ -1,30 +1,30 @@
-const supported = "share" in navigator;
-const butShare = document.getElementById("butShare");
-const divResult = document.getElementById("divResult");
+const supported = 'share' in navigator;
+const butShare = document.getElementById('butShare');
+const divResult = document.getElementById('divResult');
 
 if (supported) {
-  const divNotSupported = document.getElementById("notSupported");
-  divNotSupported.classList.toggle("hidden", true);
-  butShare.removeAttribute("disabled");
-  butShare.addEventListener("click", (e) => {
-    console.log("👍", "butShare-clicked", e);
+  const divNotSupported = document.getElementById('notSupported');
+  divNotSupported.classList.toggle('hidden', true);
+  butShare.removeAttribute('disabled');
+  butShare.addEventListener('click', (e) => {
+    console.log('👍', 'butShare-clicked', e);
     e.preventDefault();
     const shareOpts = {
-      title: "Jabberwocky",
-      text: "Check out this great poem about a Jabberwocky.",
-      url: "https://en.wikipedia.org/wiki/Jabberwocky",
+      title: 'Jabberwocky',
+      text: 'Check out this great poem about a Jabberwocky.',
+      url: 'https://en.wikipedia.org/wiki/Jabberwocky',
     };
     navigator
       .share(shareOpts)
       .then((e) => {
-        const msg = "navigator.share succeeded.";
+        const msg = 'navigator.share succeeded.';
         divResult.textContent = msg;
-        console.log("👍", msg, e);
+        console.log('👍', msg, e);
       })
       .catch((err) => {
-        const msg = "navigator.share failed";
+        const msg = 'navigator.share failed';
         divResult.textContent = `${msg}\n${JSON.stringify(err)}`;
-        console.error("👎", msg, err);
+        console.error('👎', msg, err);
       });
   });
 }
